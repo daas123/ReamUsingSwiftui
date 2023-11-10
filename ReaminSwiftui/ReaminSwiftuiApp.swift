42ecca761092c6d06668b9d1c182393bd0715030
saad <saadneosoft@gmail.com>
2023-11-10 08:45:04 +0530

    Initial Commit

---
+++ ReaminSwiftui/ReaminSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaminSwiftuiApp.swift
+//  ReaminSwiftui
+//
+//  Created by Neosoft on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ReaminSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
